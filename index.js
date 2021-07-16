d99372bc8b9065c9eda31def3317949ddcffc992
Aleksandr Schemelev <prophreaker@gmail.com>
2021-07-16 15:15:22 +0300

    feat: add data fetchinig from target url

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-const fastify = require("fastify")({ logger: true });
-const fetch = require("node-fetch");
+const fastify = require('fastify')({ logger: true });
+const fetch = require('node-fetch');
 
 const start = async () => {
   const limit =
-    typeof process.argv[2] !== "undefined" ? process.argv[2] : "100kb";
-  await fastify.register(require("fastify-express"));
-  fastify.use(require("body-parser").json({ limit }));
+    typeof process.argv[2] !== 'undefined' ? process.argv[2] : '200kb';
+    
+  await fastify.register(require('fastify-express'));
+  fastify.use(require('body-parser').json({ limit }));
 
-  fastify.get("/", async function (_, res) {
-    res.send("CORS server is running");
-  });
+  const opts = {
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+        },
+      },
+    },
+    handler: async function (request, reply) {
+      reply.header('Access-Control-Allow-Origin', '*');
+      reply.header(
+        'Access-Control-Allow-Methods',
+        'GET, PUT, PATCH, POST, DELETE'
+      );
+      reply.header('Access-Control-Allow-Headers', '*');
 
-  fastify.all("*", async function (req, res, next) {
-    console.log(res);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    console.log(req.headers, "headers");
-    res.header("Access-Control-Allow-Headers", "*");
-  });
+      if (request.method === 'OPTIONS') {
+        reply.send('CORS preflight');
+      } else {
+        const targetURL = request.headers['target-url'];
+        if (!targetURL) {
+          reply.send('There is not Target-Endpoint header in the request');
+          return;
+        }
+
+        const url = targetURL + request.url;
+
+        fetch(url, {
+          method: request.method,
+        })
+          .then((response) => response.text())
+          .then((data) => reply.send(data).code(204))
+          .catch((err) => console.log(err.message));
+      }
+    },
+  };
+
+  fastify.all('*', opts);
+
+  fastify.get('/', opts);
+
+  const PORT = 3000;
 
   try {
-    await fastify.listen(3000);
+    await fastify.listen(PORT);
+    /* console.log(`server listening at 'http://127.0.0.1:${PORT}`); */
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 };
+
 start();
